dd2fe5f4252d097f2acb00266bfda4106a9c1094
SrUsuario <raul.parada040103@gmail.com>
2023-09-10 14:27:10 +0200

    Initial Project

---
+++ src/codekonami/CodeKonami.java
@@ -0,0 +1,16 @@
+package codekonami;
+
+/**
+ *
+ * @author SrUsuario
+ */
+public class CodeKonami {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
